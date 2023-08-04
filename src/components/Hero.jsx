7c22ea4f2b2856975d1859070484c6a15c72b0b8
Philip Rey Raquinel <devphil.work@gmail.com>
2023-08-04 17:57:26 +0800

    Add Nav and Hero Content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { logo } from '../assets'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+                <img src={logo} alt='ai_writeup_snip_logo' className='w-28 object-contain' />
+
+                <button type='button' onClick={() => window.open('https://www.devphil.co')} className='black_btn'>
+                    devphil.co
+                </button>
+            </nav>
+
+            <h3 className='head_text'>
+                Summarize Articles with <br className='max-md:hidden' />
+                <span className='orange_gradient'>OpenAI GPT-4</span>
+            </h3>
+
+            <p className='desc'>
+                Simplify your reading with <strong>AI WriteUp Snip</strong>, an article summarizer that transforms lengthy articles into clear and concise summaries.
+            </p>
+        </header>
     )
 }
 
