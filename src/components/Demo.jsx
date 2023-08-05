b69bc6bf8503418ef1f778b82afe34089a9c9ddf
Philip Rey Raquinel <devphil.work@gmail.com>
2023-08-06 01:34:02 +0800

    Add URL History and Summary Results

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,17 +8,31 @@ const Demo = () => {
         summary: ''
     })
 
+    const [allArticles, setAllArticles] = useState([])
+
     const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
 
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+
+        if (articlesFromLocalStorage) {
+            setAllArticles(articlesFromLocalStorage)
+        }
+    }, [])
+
     const submitHandler = async (e) => {
-        e.prevenDefault()
+        e.preventDefault()
 
         const { data } = await getSummary({ articleUrl: article.url })
 
         if (data?.summary) {
             const newArticle = { ...article, summary: data.summary }
+            const updatedAllArticles = [newArticle, ...allArticles]
 
             setArticle(newArticle)
+            setAllArticles(updatedAllArticles)
+
+            localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
 
             console.log(newArticle)
         }
@@ -27,7 +41,6 @@ const Demo = () => {
     return (
         <section className='mt-16 w-full max-w-xl'>
             {/* Search */}
-
             <div className='flex flex-col w-full gap-2'>
                 <form className='relative flex justify-center items-center' onSubmit={submitHandler}>
                     <img src={linkIcon} alt='link_icon' className='absolute left-0 my-2 ml-3 w-5' />
@@ -45,14 +58,53 @@ const Demo = () => {
                         type='submit' 
                         className='submit_btn 
                             peer-focus:border-gray-700 
-                            peer-focus:text-gray-700'
+                            peer-focus:text-gray-700' 
+                        title='Generate Summary' 
                     >Go</button>
                 </form>
 
-                {/* Browser URL History */}
+                {/* URL history */}
+                <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+                    {allArticles.map((item, index) => (
+                        <div key={`link-${index}`} onClick={() => setArticle(item)} className='link_card'>
+                            <div className='copy_btn'>
+                                <img src={copy} alt='copy_icon' className='w-[50%] h[50%] object-contain' title='Copy to Clipboard' />
+                            </div>
+
+                            <p className='flex-1 font-satoshi text-blue-500 font-medium text-sm truncate'>
+                                {item.url}
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
 
-            {/* Display Results */}
+            {/* Display results */}
+            <div className='my-10 max-w-full flex justify-center items-center'>
+                {isFetching ? (
+                    <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+                ) : error ? (
+                    <p className='font-inter font-bold text-black text-center'>
+                        Oops, that wasn't supposed to happen...
+                        <br />
+                        <span className='font-satoshi font-normal text-gray-700'>
+                            {error?.data?.error}
+                        </span>
+                    </p>
+                ) : (
+                    article.summary && (
+                        <div className='flex flex-col gap-3'>
+                            <h2 className='font-satoshi font-bold text-gray-600 text-xl text-center'>Article <span className='blue_gradient'>Summary</span></h2>
+                            
+                            <div className='summary_box'>
+                                <p className='font-inter font-medium text-sm text-gray-700'>
+                                    {article.summary}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                )}
+            </div>
         </section>
     )
 }
